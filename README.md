90c70fb0c763f95ee23e72c64d39997020188244
visheshcreations <visheshvishwakarma5555@gmail.com>
2023-09-28 09:16:39 -0700

    starbucks-clone

---
+++ README.md
@@ -0,0 +1,3 @@
+### npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+
+### clt+j
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#1e3932",
+        orange: "#FA6A3E",
+      },
+    },
+  },
+  plugins: [],
+};
